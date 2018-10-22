47c16d9baea515265727f97cc715bf44174e3e9e
rgalazb <galaz.becerra@gmail.com>
2018-10-22 15:47:27 -0300

    codigo python comentado

--- js/script.js
+++ js/script.js
@@ -65,3 +65,66 @@ if (lostDays1 > lostDays2) {
   $('#menos-dias-cantidad').text(Math.round(lostDays1))
 }
 
+
+// Sexo = input('sexo (M/H): ')
+// Edad = int(input('edad: '))
+// Altura = float(input('altura: '))# Metros con puntos, no comas
+// Peso = int(input('peso: ')) #Kg
+// Cigarros = int(input('cigarros fumados diariamente: '))#cigarros diarios
+// DH = Edad*0.51 #Daño por edad hombre
+// DM = Edad*0.3 #Daño por edad mujer
+
+// if Sexo =='H':
+//     Cuenta = 20
+    
+//     if Cigarros<15:
+//         Cuenta+=2
+//     elif Cigarros<24:
+//         Cuenta+=4
+//     elif Cigarros>24:
+//         Cuenta+=6
+//     Cuenta += int(DH)
+    
+//     r=''
+//     if Cuenta<35:
+//         r='1%'
+//     elif Cuenta<45:
+//         r='1-3%'
+//     elif Cuenta<55:
+//         r='3-7%'
+//     elif Cuenta<65:
+//         r='7-17%'
+//     elif Cuenta<70:
+//         r='17-28%'
+//     elif Cuenta<75:
+//         r='28-40%'
+//     elif Cuenta<80:
+//         r='40-56%'
+//     print(r)
+    
+// if Sexo == 'M':
+//     Cuenta = 27
+//     if Cigarros<15:
+//         Cuenta+=8
+//     elif Cigarros<24:
+//         Cuenta+=15
+//     elif Cigarros>24:
+//         Cuenta+=18
+//     Cuenta += int(DM)
+    
+//     r=''
+//     if Cuenta<60:
+//         r='1%'
+//     elif Cuenta<70:
+//         r='1-3%'
+//     elif Cuenta<80:
+//         r='3-5%'
+//     elif Cuenta<85:
+//         r='6-11%'
+//     elif Cuenta<90:
+//         r='11-19%'
+//     elif Cuenta<95:
+//         r='19-28%'
+//     elif Cuenta<100:
+//         r='28-43%'
+//     print(r)
